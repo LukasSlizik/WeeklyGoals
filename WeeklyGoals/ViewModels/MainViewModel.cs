6a624d75170d5e915fbc1789c253e86653081e83
LukasSlizik <lukas.slizik@gmail.com>
2018-02-10 17:52:50 +0100

    Column Names added.

--- WeeklyGoals/ViewModels/MainViewModel.cs
+++ WeeklyGoals/ViewModels/MainViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections.Generic;
 using WeeklyGoals.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace WeeklyGoals.ViewModels
 {
     public class MainViewModel
     {
         public SelectList Weeks { get; set; }
+
+        [Display(Name = "Selected Week")]
         public Week SelectedWeek { get; set; }
         public IEnumerable<Progress> Progress { get; set; }
         public double RunningTotal { get; set; }
