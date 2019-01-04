51b29dabb89fe756431cdaa04e4594aca3324e91
Lukas Slizik <lukas.slizik@grollmus.de>
2019-01-04 15:08:48 +0100

    Goal Model refactored; redundant properties removed

--- WeeklyGoals/my-app/src/app/app-routing.module.ts
+++ WeeklyGoals/my-app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OverviewComponent } from './overview/overview.component';
+import { GoalComponent } from './goal/goal.component';
 
 const routes: Routes = [
   { path: '', component: OverviewComponent },
+  { path: 'create', component: GoalComponent },
 ];
 
 @NgModule({
--- WeeklyGoals/my-app/src/app/models/goal.ts
+++ WeeklyGoals/my-app/src/app/models/goal.ts
@@ -1,11 +1,8 @@
 export interface Goal {
-  id: number;
   name: string;
   description: string;
   stepSize: number;
   unit: string;
   weeklyTarget: number;
   factor: number;
-  startingYear: number;
-  startingWeek: number;
 }
