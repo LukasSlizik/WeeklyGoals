10137de1706b37ef863339161b6ee5c236bb04c8
Lukas Slizik <lukas.slizik@grollmus.de>
2019-09-04 10:59:23 +0200

    install codedeploy-agent on EC2

--- AWS Infrastructure/modules/iam/iam-role.tf
+++ AWS Infrastructure/modules/iam/iam-role.tf
@@ -1,59 +1,63 @@
-resource "aws_iam_role" "codedeploy_role" {
-  name               = "codedeploy_role"
-  assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": 
-            { 
-                "Service": "codepipeline.amazonaws.com" 
-            },
-            "Action": "sts:AssumeRole"
-        }
-    ]
-}
-EOF
-}
+# resource "aws_iam_role" "codedeploy_role" {
+#   name               = "codedeploy_role"
+#   assume_role_policy = <<EOF
+# {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Effect": "Allow",
+#             "Principal": 
+#             { 
+#                 "Service": "codepipeline.amazonaws.com" 
+#             },
+#             "Action": "sts:AssumeRole"
+#         }
+#     ]
+# }
+# EOF
+# }
 
-resource "aws_iam_role_policy" "codedeploy_policy" {
-  name = "codedeploy_policy"
-  role = "${aws_iam_role.codedeploy_role.id}"
+# resource "aws_iam_role_policy" "codedeploy_policy" {
+#   name = "codedeploy_policy"
+#   role = "${aws_iam_role.codedeploy_role.id}"
 
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement" : [
-        {
-            "Effect" : "Allow",
-            "Action" : [
-                "autoscaling:*",
-                "codedeploy:*",
-                "ec2:*",
-                "lambda:*",
-                "ecs:*",
-                "elasticloadbalancing:*",
-                "iam:AddRoleToInstanceProfile",
-                "iam:CreateInstanceProfile",
-                "iam:CreateRole",
-                "iam:DeleteInstanceProfile",
-                "iam:DeleteRole",
-                "iam:DeleteRolePolicy",
-                "iam:GetInstanceProfile",
-                "iam:GetRole",
-                "iam:GetRolePolicy",
-                "iam:ListInstanceProfilesForRole",
-                "iam:ListRolePolicies",
-                "iam:ListRoles",
-                "iam:PassRole",
-                "iam:PutRolePolicy",
-                "iam:RemoveRoleFromInstanceProfile", 
-                "s3:*"
-            ],
-            "Resource" : "*"
-        }  
-    ]
-}
-EOF
-}
+#   policy = <<EOF
+# {
+#     "Version": "2012-10-17",
+#     "Statement" : [
+#         {
+#             "Effect" : "Allow",
+#             "Action" : [
+#                 "autoscaling:*",
+#                 "codedeploy:*",
+#                 "ec2:*",
+#                 "lambda:*",
+#                 "ecs:*",
+#                 "elasticloadbalancing:*",
+#                 "iam:AddRoleToInstanceProfile",
+#                 "iam:CreateInstanceProfile",
+#                 "iam:CreateRole",
+#                 "iam:DeleteInstanceProfile",
+#                 "iam:DeleteRole",
+#                 "iam:DeleteRolePolicy",
+#                 "iam:GetInstanceProfile",
+#                 "iam:GetRole",
+#                 "iam:GetRolePolicy",
+#                 "iam:ListInstanceProfilesForRole",
+#                 "iam:ListRolePolicies",
+#                 "iam:ListRoles",
+#                 "iam:PassRole",
+#                 "iam:PutRolePolicy",
+#                 "iam:RemoveRoleFromInstanceProfile", 
+#                 "s3:*"
+#             ],
+#             "Resource" : "*"
+#         }  
+#     ]
+# }
+# EOF
+# }
+
+# resource "aws_iam_role" "codeDeployServiceRole" {
+    
+# }
--- AWS Infrastructure/stage/install/install.sh
+++ AWS Infrastructure/stage/install/install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-cd /home/ec2-user
 sudo yum -y -q update
-# sudo yum -y -q install ruby
-# sudo yum -y -q install wget
+sudo yum -y -q install ruby
+sudo yum -y -q install wget
+
+cd /home/ec2-user
 
 # Docker CE Install
 sudo amazon-linux-extras install -y docker
@@ -12,6 +13,11 @@ sudo usermod -a -G docker ec2-user
 
 # Make docker auto-start
 sudo chkconfig docker on
+
+# Code Deploy Agent
+wget https://aws-codedeploy-eu-central-1.s3.eu-central-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
 
 # sudo yum remove docker docker-engine docker.io containerd runc
 # sudo yum install -y -q \
